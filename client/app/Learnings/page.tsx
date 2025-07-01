7dd2c39c65364c99c57b03746309effdfccef705
navneetshahi14 <navneet.shahi2004@gmail.com>
2025-07-01 16:49:27 +0530

    logo upload

--- client/app/Learnings/page.tsx
+++ client/app/Learnings/page.tsx
@@ -83,6 +83,7 @@ export default function Page() {
         const res = await data.json();
         console.log(res);
         setResd(res)
+        setClose(false);
         setIsloading(false);
     }
 
